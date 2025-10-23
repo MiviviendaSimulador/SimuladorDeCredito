ea3822fe336de855a56b8e6e6d18913afc209bfb
jsyumg <u202210838@upc.edu.pe>
2025-10-22 19:00:11 -0500

    first commit

---
+++ src/main/java/com/mivivienda/platform/simuladordecredito/SimuladorDeCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.mivivienda.platform.simuladordecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorDeCreditoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimuladorDeCreditoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mivivienda/platform/simuladordecredito/SimuladorDeCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mivivienda.platform.simuladordecredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorDeCreditoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
